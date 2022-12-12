52f2abdd385ea1d5663c8445546361b95305c3c0
audreyleh <110577011+audreyleh@users.noreply.github.com>
2022-12-12 19:11:09 -0300

    Update sql_2609.sql

--- sql_2609.sql
+++ sql_2609.sql
@@ -2,3 +2,11 @@
 	FROM categories, products
 	where products.id_categories = categories.id
 	GROUP BY categories.name
+	
+	
+/*  select categories.name, SUM(products.amount)
+	FROM categories, products
+	where products.id_categories = categories.id
+	GROUP BY categories.name
+	order by categories.name asc
+*/
